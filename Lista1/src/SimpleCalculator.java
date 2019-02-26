b458edf2e47c008b0ff1a241455b07063c9bc3d1
TARCISIO CANTALICE PEREIRA BORGES <200003867@ucsal.br>
2019-02-26 19:28:17 -0300

    Questão 3

--- Lista1/src/SimpleCalculator.java
+++ Lista1/src/SimpleCalculator.java
@@ -7,28 +7,41 @@ import java.util.Scanner;
 public class SimpleCalculator {
 
     public static Scanner input = new Scanner(System.in);
-
-    public static String inputValues(){
-        StringBuilder operation = new StringBuilder();
-        boolean hasEqual = false;
-        do{
-            System.out.print("-> ");
-            String line = SimpleCalculator.input.nextLine();
-            line = line.replaceAll(" ", "");
-            operation.append(line);
-            
-            if(operation.indexOf("=") != -1){
-                hasEqual = true;
-            }
-            else{
-                System.out.printf("%s\n", operation.toString());
-            }
-        }while(!hasEqual);
-        return operation.toString().replace("=", "");
+    
+    public static double inputNumber(){
+	System.out.print(">> ");
+	return new Double(SimpleCalculator.input.nextLine());
     }
 
+    public static char inputOperation(){
+	System.out.print(">> ");
+	return SimpleCalculator.input.nextLine().charAt(0);
+    }
+
+    public static double calculate(double number1, double number2, char operation){
+	double result 0.0;
+	switch(operation){
+	    case '+':
+		result = number1 + number2;
+		break;
+	    case '-':
+		result = number1 - number2;
+		break;
+	    case '*':
+		result = number1 * number2;
+		break;
+	    case '/':
+		if(number2 != 0.0){
+		    result = number1 / number2:
+		}
+		break;
+	}
+	return result;
+    }
     public static void main(String[] args){
-        String operation = SimpleCalculator.inputValues();
-        System.out.println(operation);
+    	double number1 = SimpleCalculator.inputNumber();
+	char operation = SimpleCalculator.inputOperation();
+	double number2 = SimpleCalculator.inputNumber();
+	System.out.printf(":>> %.2", SimpleCalculator.calculate(number1, number2, operation));
     }
 }
